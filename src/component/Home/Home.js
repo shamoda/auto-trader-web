9c6ee588e9677b9abb5c649509d3bbacb8e66982
shamoda <it19161648@my.sliit.lk>
2021-08-19 07:17:56 +0530

    pipeline should work

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends Component {
         return ( 
             <div>
                 <div className="spare-banner">
-                    <h1>Finally... Pipeline Works!!!! :D</h1>
+                    <h1>Drop the Gear and Disappear...</h1>
                 </div>
                 <div>
                     {/* <Container className="admindash-container"> */}
